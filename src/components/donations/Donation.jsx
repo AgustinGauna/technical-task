6edd0823d94995fa2c9945a46fdd5c045d9ab649
AgustinGauna <agustingauna1997@gmail.com>
2024-02-02 17:10:50 -0300

    Added responsive design

--- src/components/donations/Donation.jsx
+++ src/components/donations/Donation.jsx
@@ -1,15 +1,15 @@
-import React from "react";
-import "./donation.scss";
-import Button from "../Button";
+import React from 'react';
+import './donation.scss';
+import Button from '../Button';
 
 const Donation = ({ isMonthly }) => {
   return (
-    <div className="donation-container">
-      <div className="top">
+    <div className='donation-container'>
+      <div className='top'>
         <p>I would like to make a monthly donation of</p>
       </div>
 
-      <div className="donation-buttons">
+      <div className='donation-buttons'>
         <Button amount={6} selected={false} />
 
         <Button amount={12} selected={true} />
@@ -19,42 +19,46 @@ const Donation = ({ isMonthly }) => {
         <Button amount={30} selected={false} />
 
         <button
-          className="custom-amount"
-          type="number"
-          placeholder="Other amount"
+          className='custom-amount'
+          type='number'
+          placeholder='Other amount'
         >
-          <img className="pound" src="pound.svg" alt="pound sign" />
+          <img className='pound' src='pound.svg' alt='pound sign' />
           <span>Other amount</span>
         </button>
 
-        <button className="submit-button">
+        <button className='submit-button'>
           <span>Donate </span>
           <span>
-            <img className="pound" src="pound.svg" alt="pound sign" />
+            <img className='pound' src='pound.svg' alt='pound sign' />
             12
           </span>
-          <span>{isMonthly ? "monthly" : "today"}</span>
-          <img className="secure" src="secure.svg" alt="" />
+          <span className='submit-button-secure'>
+            {isMonthly ? 'monthly' : 'today'}
+            <img className='secure' src='secure.svg' alt='' />
+          </span>
         </button>
       </div>
 
-      <div className="message">
+      <div className='message'>
         <p>
           £16 Could help an Animal Rescue Team take on an urgent animal rescue.
         </p>
       </div>
 
-      <div className="payment-info">
-        <p>
+      <div className='payment-info'>
+        <p className='payment-text'>
           {isMonthly
-            ? "I would like to make a one-off donation"
-            : "I would like to give monthly"}
+            ? 'I would like to make a one-off donation'
+            : 'I would like to give monthly'}
         </p>
 
-        <div>
+        <div className='payment-details'>
           <p>All Direct Debits are protected by the Direct Debit Guarantee</p>
-          <img src="direct-debit-1-logo-png-transparent.png" alt="" />
-          <img src="fundraising-regulator-logo.png" alt="" />
+          <div className='payment-images'>
+            <img src='direct-debit-1-logo-png-transparent.png' alt='' />
+            <img src='fundraising-regulator-logo.png' alt='' />
+          </div>
         </div>
       </div>
     </div>
